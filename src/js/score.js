dc26bc58708edb6ed0dc079cac67e300f962a2dd
thomascconstant <thoams.c.constant@gmail.com>
2016-07-28 15:17:36 +0200

    suppression console.log

--- src/js/score.js
+++ src/js/score.js
@@ -1,4 +1,3 @@
 document.getElementById("resultTom").innerHTML = localStorage.scoreJoueurTom;
-console.log(localStorage.scoreJoueurTom + "hop");
 document.getElementById("resultChristopher").innerHTML = localStorage.scoreJoueurChristopher;
 document.getElementById("resultHugh").innerHTML = localStorage.scoreJoueurHugh;
