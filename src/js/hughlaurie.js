d2954960ce33e7f57b78230204435a35bf1c85c4
thomascconstant <thoams.c.constant@gmail.com>
2016-08-02 14:03:04 +0200

    nettoyage code

--- src/js/hughlaurie.js
+++ src/js/hughlaurie.js
@@ -9,7 +9,7 @@ var figures = [
     "../src/img/haha.svg",
     "../src/img/meh.svg",
     "../src/img/cheveux.svg",
-    "../src/img/smile.svg",]
+    "../src/img/smile.svg"]
 var order = [0,1,2,3,4,5,6];
 var me;
 var him;
@@ -76,26 +76,26 @@ function showMise() {
 }
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
   return array;
 }
 
 function shuffleOrder(){
-        order = shuffle(order);        
+    order = shuffle(order);        
 }
 
 function doIBeatHim(me, him) {
