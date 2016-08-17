b53efa31090201aee9e18a7dc0fb476b98b410d8
thomascconstant <thomas.c.constant@gmail.com>
2016-08-17 16:57:51 +0200

    ajout fin du jeu queen.html; modification calcul du score et génération dans l'antre et en fin de jeu score.js; modification du chargement de la fonction de calcul du score au chargement de la page hub.html

--- src/js/score.js
+++ src/js/score.js
@@ -1,20 +1,28 @@
 var scoreGlobal=localStorage.scoreJoueurChristopher + localStorage.scoreJoueurTom + localStorage.scoreJoueurHugh;
 console.log(scoreGlobal + " moutons sauvés");
 
-document.getElementById("resultTom").innerHTML = localStorage.scoreJoueurTom;
-document.getElementById("resultChristopher").innerHTML = localStorage.scoreJoueurChristopher;
-document.getElementById("resultHugh").innerHTML = localStorage.scoreJoueurHugh;
-
 function feedbackScore () {
+    document.getElementById("resultTom").innerHTML = localStorage.scoreJoueurTom;
+    document.getElementById("resultChristopher").innerHTML = localStorage.scoreJoueurChristopher;
+    document.getElementById("resultHugh").innerHTML = localStorage.scoreJoueurHugh;
+}
+
+
+function feedbackScoreGeneral () {
     if (scoreGlobal >= 10 && scoreGlobal < 50) {
-        document.getElementById("??").innerHTML = "Vous avez sauvé " + scoreGlobal + "moutons. Vous faites un bon berger, mais la Reine attendait mieux.";
-    } else if (scoreGlobal > 0 && scoreGlobal < 10) {
-        document.getElementById("??").innerHTML = "Vous avez sauvé " + scoreGlobal + "moutons. La Reine s'en contentera.";
+        scoreGlobal = Math.abs(scoreGlobal);
+        document.getElementById("feedbacks").innerHTML = "« Vous avez sauvé " + scoreGlobal + " de mes sujets.<br/>Vous faites un bon berger mais j'attendais mieux de votre part.<br/> Vous m'aviez pourtant été chaudement recommandé par le Prince Gustave... »";
     } else if (scoreGlobal === 1) {
-        document.getElementById("??").innerHTML = "Vous avez sauvé " + scoreGlobal + "mouton. C'est mieux que rien, mais le Royaume n'en survivra pas.";
+        scoreGlobal = Math.abs(scoreGlobal);
+        document.getElementById("feedbacks").innerHTML = "« Il semblerait que vous n'ayez sauvé qu' " + scoreGlobal + " seul de mes sujets.<br/>Vous savez vraiment à quoi ressemble un mouton ? »";
+    } else if (scoreGlobal > 0 && scoreGlobal < 10 && scoreGlobal !== 1) {
+        scoreGlobal = Math.abs(scoreGlobal);
+        document.getElementById("feedbacks").innerHTML = "« Vous avez sauvé " + scoreGlobal + " de mes sujet.<br/>J'imagine que cela vaut mieux que rien, mais le Royaume n'en survivra pas ! »";
     } else if (scoreGlobal >= 50) {
-        document.getElementById("??").innerHTML = "Vous avez sauvé " + scoreGlobal + "moutons. Votre CV n'a pas menti, vous êtes un excellent berger.<br/> La Reine vous tient en haute estime et vous fournira une lettre de recommandation";
+        scoreGlobal = Math.abs(scoreGlobal);
+        document.getElementById("feedbacks").innerHTML = "« Vous avez sauvé " + scoreGlobal + " de mes sujets.<br/>Votre curriculum vitae n'a pas menti, vous êtes un excellent berger. »<br/><br/>Avant de vous laisser partir, la Reine vous tend une lettre de recommandation.";
     } else if (scoreGlobal < 0) {
-        document.getElementById("??").innerHTML = "Vous avez condamné " + scoreGlobal + "moutons. Le Sorcier vous félicite : à la fin du méchoui, il n'y aura plus âme qui vive dans le Royaume.<br/> A l'exception de la Reine, bien sûr. Mais vous devriez éviter de la croiser.";
+        scoreGlobal = Math.abs(scoreGlobal);
+        document.getElementById("feedbacks").innerHTML = "« Vous avez condamné " + scoreGlobal + " de mes sujets !<br/>Sur ce parchemin, le Sorcier lui-même vous félicite :<br/>il n'y aura plus âme qui vive dans le Royaume ! »<br/><br/>Avant de vous laisser partir, la Reine vous transmet un dernier message :<br/>une offre d'emploi dans l'antre du Sorcier.";
     }
 }
