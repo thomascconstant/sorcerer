73db9c15cdb44e4d37add12a3c5afc0fb6121e91
thomascconstant <thomas.c.constant@gmail.com>
2016-08-17 15:25:16 +0200

    si le joueur a terminé toutes les épreuves, une nouvelle page s'ouvre vers la fin du jeu (queen.html) (main.js); ajoue d'une variable de calcul du score global du joueur (score.js)

--- src/js/score.js
+++ src/js/score.js
@@ -1,3 +1,6 @@
+var scoreGlobal=localStorage.scoreJoueurChristopher + localStorage.scoreJoueurTom + localStorage.scoreJoueurHugh;
+console.log(scoreGlobal + " moutons sauvés");
+
 document.getElementById("resultTom").innerHTML = localStorage.scoreJoueurTom;
 document.getElementById("resultChristopher").innerHTML = localStorage.scoreJoueurChristopher;
 document.getElementById("resultHugh").innerHTML = localStorage.scoreJoueurHugh;
