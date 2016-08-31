7f920b2c3f3aec53170627508b709d89dd130bd6
thomascconstant <thomas.c.constant@gmail.com>
2016-08-31 17:10:35 +0200

    nouveau script php pour version en ligne

--- src/php/toto.php
+++ src/php/toto.php
@@ -1,15 +1,35 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-// à commenter pour la version en ligne
-function openFile () {
-    return fopen("../data.csv", "a");
+function enregistrer($data) {
+    //$dest = "ftp://username:password@ftp.mywebsite.com/path/to/the/file/data.csv"; // à décommenter pour la version en ligne
+    $dest = "../data.csv"; // à commenter pour la version en ligne
+    
+    $filedata = file_get_contents($dest);
+    $filedata.= $data;
+
+    $streamcontext = stream_context_create(array('ftp' => array('overwrite' => true)));
+
+    $result = file_put_contents($dest, $filedata, 0, $streamcontext);
+    if ($result === FALSE) {
+           echo "erreur";     
+    } else {
+        echo "success";
+    }
+
 }
 
-// à décommenter pour la version locale
-/*function openFile () {
-    return fopen("ftp://username:password@monsite.com/www/sorcerer/data.csv", "a");
-}*/
+if (isset($_REQUEST['data'])) {
+    enregistrer($_REQUEST['data']);
+} else {
+    echo "erreur data"; 
+}
 
+// old version local, fonctionnelle
+/*function openFile () {
+    return fopen("../data.csv", "a");
+}
 
 function enregisterJoueur ($joueur) {
     $handle = openFile();
@@ -20,12 +40,13 @@ function enregister ($data) {
     $handle = openFile();
     fwrite($handle, $data);
 }
-
+    
 if (isset($_REQUEST['joueur'])) {
     enregisterJoueur($_REQUEST['joueur']);
 } else if (isset($_REQUEST['data'])) {
     enregister($_REQUEST['data']);
 } else if (isset($_REQUEST['score'])) {
     enregister($_REQUEST['score']);
-}
+}*/
+
 ?>
