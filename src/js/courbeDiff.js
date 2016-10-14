be40417af32eda47bb54c710818f42e5a4a720e7
thomascconstant <thomas.c.constant@gmail.com>
2016-10-14 14:54:52 +0200

    modification de la difficulté

--- src/js/courbeDiff.js
+++ src/js/courbeDiff.js
@@ -1,4 +1,5 @@
 ﻿var difficulty = 0;
+var mode = 0; //à intégrer dans chaque jeu, 0 pour adaptation de la difficulté en fonction win/fail, 1 pour courbe bonds
 
 /* variables à récupérer : 
  * winState
@@ -7,6 +8,18 @@
  * difficulty (gameSpeed dans tomcruise.js)
  */
 
+// à intégrer dans chaque jeu
+function changeMetaDiff() {
+    if (mode === 0) {
+        // reprendre code actuel fonctionnement diff
+    } else if (mode === 1) {
+        // envoyer vers contenu de ce script js
+        cumulTours();
+        checkWinFail();
+        changeDifficulty();
+    }
+}
+
 // conserver le nombre de tours gagnants d'affilé
 var lastWinFail = [];
 var cumulWin = [];
@@ -14,6 +27,7 @@ var cumulFail = [];
 var bondDiff = [0.1, 0.3, 0.7];
 var win = "true";
 var fail = "false";
+
 var compteurTroisTours = 0; //compteur qui augmente après chaque tour
 var toursGagnantsDaffile = 0; //à incrémenter, dés que c'est égal à 3, modifier courbe
 
@@ -44,38 +58,58 @@ function checkWinFail() {
         i++;
     }
     console.log(cumulWin);
+
+    var idx2 = lastWinFail.indexOf(win);
+    var j = 0;
+    while (idx2 != -1 && j >= 3) {
+        cumulFail.push(idx2);
+        idx2 = lastWinFail.indexOf(win, idx2 + 1);
+        j++;
+    }
+    console.log(cumulFail);
 }
 
-var compareArrayWin = function (lastWinFail, cumulWin) {
-    if (lastWinFail === cumulWin)
-        return true;
-    if (lastWinFail.length !== cumulWin.length)
-        return false;
-    for (var i = 0; i < lastWinFail.length; i++) {
-        if (!equal(lastWinFail[i], cumulWin[i])) return false;
-    };
-    return true;
-};
 
-var compareArrayFail = function (lastWinFail, cumulFail) {
-    if (lastWinFail === cumulFail)
-        return true;
-    if (lastWinFail.length !== cumulFail.length)
-        return false;
-    for (var i = 0; i < lastWinFail.length; i++) {
-        if (!equal(lastWinFail[i], cumulFail[i])) return false;
-    };
-    return true;
-};
 
 function changeDifficulty() {
+    //comparer les tableaux pour vérifier la présence de trois win
+    var compareArrayWin = function (lastWinFail, cumulWin) {
+        if (lastWinFail === cumulWin) {
+            return true;
+        }
+        if (lastWinFail.length !== cumulWin.length) {
+            return false;
+        }
+        for (var i = 0; i < lastWinFail.length; i++) {
+            if (!equal(lastWinFail[i], cumulWin[i])) return false;
+        }
+        return true;
+    };
+
+    //comparer les tableaux pour vérifier la présence de trois fail
+    var compareArrayFail = function (lastWinFail, cumulFail) {
+        if (lastWinFail === cumulFail) {
+            return true;
+        }
+        if (lastWinFail.length !== cumulFail.length) {
+            return false;
+        }
+        for (var i = 0; i < lastWinFail.length; i++) {
+            if (!equal(lastWinFail[i], cumulFail[i])) return false;
+        }
+        return true;
+    };
+
+    //modifier la difficulté en fonction
     if (compareArrayWin === true) {
         newDiff = Math.random(bondDiff);
-        difficulty = newDiff;
-    } else if (compareArrayFail === false) {
+        difficulty = difficulty + newDiff;
+    } else if (compareArrayFail === true) {
         newDiff = Math.random(bondDiff);
-        difficulty = (- newDiff);
+        difficulty = difficulty - newDiff;
     } else {
+
 
     }
 }
+
